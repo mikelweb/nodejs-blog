83a89d15b1ac41f861eac734c5a6098c72dbcadd
mikelweb <info@mikelweb.net>
2025-10-05 17:52:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ app.use(cors());
 // Convert body to JS Obj
 app.use(express.json());
 
+// Routes
+app.get("/probando", (req, res) => {
+    console.log("Se ha ejecutado el endpoint /probando");
+
+    return res.status(200).send(`
+        <h1>Endpoint /probando</h1>
+    `);
+})
+
 // Run server and listen on port 3900
 app.listen(port, () => {
     console.log("Server running on port " + port)
