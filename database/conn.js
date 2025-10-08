a685915a91165a26e91dfcaee15aeb90e6b4e02e
mikelweb <info@mikelweb.net>
2025-10-08 17:58:39 +0200

    Update conn.js

--- database/conn.js
+++ database/conn.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 
 const conn = async() => {
+    try {
         await mongoose.connect("mongodb+srv://testDB:testDB@pruebas.ipbyxfk.mongodb.net/mi_blog");
         console.log("Connected to DB");
+    } catch(error) {
+        console.log(error);
+        throw new Error("Failed to connect DB");
+    }
 }
 
 module.exports = {
