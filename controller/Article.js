a3e0280e3001e80db9228a458e6694b30ca220ba
mikelweb <info@mikelweb.net>
2025-10-10 12:15:50 +0200

    Update Article.js

--- controller/Article.js
+++ controller/Article.js
@@ -2,9 +2,28 @@ const prueba = (req, res) => {
     return res.status(200).json({
         mensaje:"Mensaje de prueba"
     })
+}
 
+const cursos = (req, res) => {
+    console.log("Se ha ejecutado el endpoint /cursos");
+
+    return res.status(200).json(
+        [
+            {
+                "nombre":"Python",
+                "horas":25,
+                "dificultad":"media"
+            },
+            {
+                "nombre":"NodeJS",
+                "horas":40,
+                "dificultad":"media"
+            }
+        ]
+    );
 }
 
 module.exports = {
-    prueba
+    prueba,
+    cursos
 }
