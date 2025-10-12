720ff65ff12a38e6f86566e6f0b27f0668006d8d
mikelweb <info@mikelweb.net>
2025-10-12 12:18:19 +0200

    Update article.js

--- routes/article.js
+++ routes/article.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const ArticleController = require("../controller/Article")
 
 router.get("/ruta-de-prueba", ArticleController.prueba)
+router.get("/cursos", ArticleController.cursos)
 
 module.exports = router;
