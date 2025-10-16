37859aa9bc8f518681080c83238350177ea54da9
mikelweb <info@mikelweb.net>
2025-10-16 13:37:22 +0200

    route to getArticles method

--- routes/article.js
+++ routes/article.js
@@ -6,5 +6,6 @@ const ArticleController = require("../controller/Article")
 router.get("/ruta-de-prueba", ArticleController.prueba);
 router.get("/cursos", ArticleController.cursos);
 router.post("/crear", ArticleController.createArticle);
+router.get("/articulos", ArticleController.getArticles);
 
 module.exports = router;
