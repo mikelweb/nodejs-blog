49cd161ef3f9a35e5fddf87641c3ff3649a0c460
Mikel Goyeneche <info@mikelweb.net>
2025-11-04 18:20:45 +0100

    Update article.js

--- routes/article.js
+++ routes/article.js
@@ -4,6 +4,14 @@ const ArticleController = require("../controller/Article")
 
 const router = express.Router();
 
+const myStorage = multer.diskStorage({
+    destination: function(req, res, cb) {
+        cb(null, "./files/img")
+    },
+    filename: function(req, file, cb) {
+        cb(null, "article_" + Date.now() + "_" + file.originalname);
+    }
+});
 
 router.post("/crear", ArticleController.createArticle);
 router.get("/articulos", ArticleController.getArticles);
